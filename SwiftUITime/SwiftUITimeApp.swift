e8fd2eb7c13ad5f8c2e3a3fb62322094a548de68
Nicholas Mandelbaum <nicholas@wehavesun.com>
2024-10-25 14:58:47 +0100

    Initial Commit

---
+++ SwiftUITime/SwiftUITimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITimeApp.swift
+//  SwiftUITime
+//
+//  Created by Nicho on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
